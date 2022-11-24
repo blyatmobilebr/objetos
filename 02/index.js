5267bf4be7b1bf34a64f0a41df913d9f6e519fa7
Кариша <lucaslima2458@gmail.com>
2022-11-24 13:47:28 -0300

    chore: exrcício 02 resolvido

---
+++ 02/index.js
@@ -0,0 +1,28 @@
+const carro1 = [
+    {
+        marca: "Volkswagen",
+        modelo: "Gol",
+        ano: 2022,
+        cor: "vermelho",
+        quantidade_portas: 4,
+        automatico: false
+    },
+
+    {
+        marca: "Fiat",
+        modelo: "Palio",
+        ano: 2020,
+        cor: "Preto",
+        quantidade_portas: 2,
+        automatico: true
+    },
+
+    {
+        marca: "Ferrari",
+        modelo: "Enzo",
+        ano: 2005,
+        cor: "vermelho",
+        quantidade_portas: 2,
+        automatico: false
+    }
+];
