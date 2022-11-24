72e30e488576de2a103cc3607465f58c034b571e
Кариша <lucaslima2458@gmail.com>
2022-11-24 13:47:14 -0300

    chore: exrcício 01 resolvido

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Rua Nome Aleatório",
+    numero: "111",
+    complemento: "Casa",
+    bairro: "Qualquer",
+    cep: "11111-111",
+    cidade: "Curitiba",
+    estado: "PR",
+    pais: "Brasil"
+};
+
+
+const usuario = {
+    nome: "Aleatório",
+    email: "teste@proton.me",
+    telefone: "91111-1111",
+    data_nascimento: "11/11/00",
+    endereco
+}
+
