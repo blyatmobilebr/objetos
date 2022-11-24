8f261d9019fefcd2cf557b00f1edd5783b827bbe
Кариша <lucaslima2458@gmail.com>
2022-11-24 13:49:11 -0300

    chore: exrcício 09 resolvido

---
+++ 09/index.js
@@ -0,0 +1,27 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+];
+
+
+let resposta = participantes.findIndex(pessoa => {
+    return pessoa.nome === "Carlos"
+});
+
+console.log(`Gente, o Carlos está na posição ${resposta + 1}, corre lá!`)
